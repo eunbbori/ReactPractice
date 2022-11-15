c3a66178e407a6903af6ffba9ece6ec6e78985b7
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2022-11-15 22:51:05 +0900

    initial

--- src/Say.js
+++ src/Say.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const Say = () => {
   const [ message, setMessage] = useState('');
   const onClickEnter = () => setMessage('안녕하세요!');
-  const onClickLeave = () => setMessage('안녕히 가세s요!'); 
+  const onClickLeave = () => setMessage('안녕히 가세요!'); 
 
   return (
     <div> 
